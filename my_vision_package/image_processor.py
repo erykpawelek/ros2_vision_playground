7304373d11c1e1aaa589ace1c993db8f8a3379b3
erykpawelek <panzereryk03@gmail.com>
2025-11-20 14:05:48 +0000

    Color recognition added

--- my_vision_package/image_processor.py
+++ my_vision_package/image_processor.py
@@ -1,46 +1,49 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import CompressedImage,Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
 class ImageProcessor(Node):
     def __init__(self):
-        super().__init__('image_prcessor')
+        super().__init__('image_processor')
 
-        # Bridge initialization 
         self.bridge = CvBridge()
-
-        # Subscription initialization
         self.subscription = self.create_subscription(
             CompressedImage,
             '/camera/image_raw/compressed',
             self.listener_callback,
             10)
-        # Publisher initialization
-        self.publisher_ = self.create_publisher(Image, '/image_processed', 10)
+        self.publisher_ = self.create_publisher(CompressedImage, '/image_processed/compressed', 10) 
     
     def listener_callback(self, msg):
         try:
-            
-            np_arr = np.frombuffer(msg.data, np.uint8)
-            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            np_arr = np.frombuffer(msg.data, np.uint8)              # Converting incoming bytes into np.array object for cv2
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)       # Converting one dimmensional array into multidimensional
+            height, width, channels = cv_image.shape                # Collecting data about image processed by cv_bridge
+            hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)   # Converting to HSV because it separates color form brightness, making detection more robust to lighting changes.
 
-            height, width, channels = cv_image.shape
+            lower_blue = np.array([95, 80, 50])                     # Lower limit for blue color range
+            upper_blue = np.array([140, 255, 255])                  # Upper limit for blue color range
 
-            center_coordinates = (width // 2, height // 2)
-            radius = 50
-            color = (0, 255, 0)
-            thickness = 5
-            cv2.circle(cv_image, center_coordinates, radius, color, thickness)
+            mask = cv2.inRange(hsv_image, lower_blue, upper_blue)   # Generating black-white mask, where blue = white  
+            masked_image = cv2.bitwise_and(cv_image, cv_image, mask= mask)
 
-            self.get_logger().info(f'Processed a frame: {width}x{height}')
+            white_pixels = cv2.countNonZero(mask)
+            self.get_logger().info(f'I can see {white_pixels} blue pixels.')
 
-            output_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+            success, encoded_jpg = cv2.imencode('.jpg', masked_image, [int(cv2.IMWRITE_JPEG_QUALITY), 50])
 
-            self.publisher_.publish(output_msg)
-        
+            if success:
+                output_msg = CompressedImage()
+                output_msg.header = msg.header
+                output_msg.format = "jpeg"
+                output_msg.data = encoded_jpg.tobytes()
+
+                self.publisher_.publish(output_msg)
+
+
         except Exception as e:
             self.get_logger().info(f'Processing ERROR: {e}')
 
@@ -55,5 +58,5 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         main()
