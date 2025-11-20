76f3f8fa7373be0e6685f2716f73abdf17812ec7
erykpawelek <panzereryk03@gmail.com>
2025-11-20 20:44:31 +0000

    Parametrization and launch file

---
+++ launch/vision_launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription    
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='my_vision_package',
+            executable='image_processor',
+            name='image_processor',
+            output='screen',
+            emulate_tty=True,
+            parameters=[
+                {'h_upper': 140},
+                {'h_lower': 95},
+                {'s_lower': 90},
+            ]
+        ),
+
+        Node(
+            package='camera_ros',
+            executable='camera_node',
+            name='camera',
+            output='screen',
+            emulate_tty=True,
+            parameters=[]
+        )
+    ])
